ffe3a89f53e648a724c073e15a48863dfa3daac2
Parth Joshi <pjoshi4@lakeheadu.ca>
2021-03-20 19:50:50 -0400

    Login and register using Firebase Auth

---
+++ app/src/main/java/com/example/comp3025assignment/SignInActivity.kt
@@ -0,0 +1,60 @@
+package com.example.comp3025assignment
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+@Suppress("DEPRECATION")
+class SignInActivity: AppCompatActivity() {
+        val RC_SIGN_IN=1234
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_sign_in)
+
+
+            // Choose authentication providers
+            val providers = arrayListOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    //AuthUI.IdpConfig.PhoneBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build())
+                    //AuthUI.IdpConfig.FacebookBuilder().build(),
+                    //AuthUI.IdpConfig.TwitterBuilder().build())
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN)
+        }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent=Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Toast.makeText(this, "Sign-in Failed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
